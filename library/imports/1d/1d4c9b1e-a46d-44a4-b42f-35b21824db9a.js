54782038ba5f91e886e95935fafef1fee3cefbd2
Jason Lee <jawc@hotmail.com>
2018-07-29 21:08:46 +0800

    update

---
+++ library/imports/1d/1d4c9b1e-a46d-44a4-b42f-35b21824db9a.js
@@ -0,0 +1,53 @@
+"use strict";
+cc._RF.push(module, '1d4c9sepG1EpLQvNbIYJNua', 'start_game');
+// scripts/start_game.js
+
+'use strict';
+
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad: function onLoad() {
+        this.node.on('mousedown', function (event) {
+            console.log('Hello!');
+            cc.director.loadScene("game");
+        });
+    },
+
+    start: function start() {}
+}
+
+// update (dt) {},
+);
+
+cc._RF.pop();
